1e281c7831b0df1032929a3154771d9a5ecee162
rithue <rithue2000@gmail.com>
2024-05-21 13:15:52 -0700

    add updates to work w mongo explain output format

--- convertJsonToCollapsed.py
+++ convertJsonToCollapsed.py
@@ -15,16 +15,16 @@ def save(file_name, traces):
     new_file_name = file_name + ".txt"
     with open (new_file_name, 'w') as f:
        for line in traces:
-           f.write(line.encode('utf-8') + " 1" + "\n")
+           f.write(str(line.encode('utf-8')) + " 1" + "\n")
 
 def walk_tree(data, stack, traces):
     if not data:
         return
-    
     for k, v in data.items():
         newStack = stack + ";" + k
         traces.append(newStack)
-        walk_tree(v, newStack, traces)
+        if (type(v) == dict):
+          walk_tree(v, newStack, traces)
 
 def parse_input():
     if len(sys.argv) != 2:
